bc3f4dd32c8abe55dffca016c23404f72f7ca24b
alper-d <44575998+alper-d@users.noreply.github.com>
2025-04-03 11:55:36 +0300

    Change pruning ratio

--- configurations.py
+++ configurations.py
@@ -18,7 +18,7 @@ def get_argparser():
 os.environ["BREVITAS_JIT"] = "1"
 argparser = get_argparser()
 # pruning_amount = argparser.pruning_amount
-pruning_amount = [0.0, 0.0] + [0.5] * 6 + [0.0]
+pruning_amount = [0.0, 0.0] + [0.5] * 4 + [0.0] * 3
 run_netron = argparser.run_netron
 pruning_mode = argparser.pruning_mode
 
